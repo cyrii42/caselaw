04ae68416cba2a542d2122409c3f999d8e259e88
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2024-09-19 00:47:58 -0400

    added some tests

--- convert_dumb_to_smart_quotes.py
+++ convert_dumb_to_smart_quotes.py
@@ -50,3 +50,45 @@ def dumb_to_smart_quotes_v2(string):
 
 #     with open(f"{i}.md", "w") as f:
 #         f.write(new_text)
+
+print("\N{LEFT DOUBLE QUOTATION MARK}")
+print("\N{RIGHT DOUBLE QUOTATION MARK}" == "”")
+
+test_str = '''
+#### Individualized Issues
+- [*Patel v. Trans Union, LLC*](https://www.westlaw.com/Document/I49d881801e5411e5be1ff4cec5913d5d/View/FullText.html?transitionType=Default&contextData=(sc.Default)&VR=3.0&RS=cblt1.0), 308 F.R.D. 292, 302 (N.D. Cal. 2015)
+  > The issue of accuracy is a core issue for ascertainability and the predominance of common issues. The defendants' fundamental quarrel is with the plaintiff's assertion that tagging the class members as (for example) 'terrorists' (via the name-only logic) is always inaccurate and thus is enough to define the class (and establish the predominance of this common issue). The defendants counter that the plaintiff cannot show, except file by file, that the SmartMove reports wrongly tagged the class members as terrorists. They complain that the plaintiff cannot just assert that the terrorist tags were inaccurate and then shift the burden to the defendants to prove that the tags were accurate.
+  > 
+  > Normally, the defendants would be right. But this case presents a peculiar situation. The defendants' implied argument is that a significant number of the proposed class actually may have been accurately tagged as potential terrorists. Absent some pretty significant proof to the contrary, the court is willing to assume that no significant (read: certification-breaking) fraction of the tagged proposed class was in fact accurately tagged as potential terrorists.
+
+#### Hypothetical scenarios
+
+- [*Van v. LLR, Inc.*](https://www.westlaw.com/Document/I8535be00c1d511edb30aae965a5264be/View/FullText.html?transitionType=Default&contextData=(sc.Default)&VR=3.0&RS=cblt1.0), 61 F.4th 1053, 1067-68 (9th Cir. 2023)
+  > ***If the plaintiff demonstrates that class issues exist, the defendant must invoke individualized issues and provide sufficient evidence that the individualized issues bar recovery on at least some claims, thus raising the spectre of class-member-by-class-member adjudication of the issue.*** See True Health Chiropractic, Inc. v. McKesson Corp., 896 F.3d 923, 932 (9th Cir. 2018) (“[W]e do not consider ... defenses that [the defendant] might advance or for which it has presented no evidence.” (emphasis added)).
+  >
+  > If the defendant provides evidence that a valid defense—affirmative or otherwise—will bar recovery on some claims, then the district court must determine, based on the particular facts of the case, “whether individualized questions ... ‘will overwhelm common ones and render class certification inappropriate under Rule 23(b)(3).’ ” Olean, 31 F.4th at 669 (quoting Halliburton Co. v. Erica P. John Fund, Inc., 573 U.S. 258, 276, 134 S.Ct. 2398, 189 L.Ed.2d 339 (2014))
+  >
+  > As pertains to the voluntary payment issue raised by LuLaRoe, LuLaRoe failed to provide sufficient evidence that any class member would lack a meritorious claim on this basis. Only a handful of Alaskan invoices submitted to the district court demonstrate that the purchaser knew of the sales tax issue before completing the purchase. And on each of these invoices, the purchaser was provided a discount on the transaction equal to or greater than the amount of improper sales tax, with at least some of the discounts provided for the express purpose of offsetting the sales tax.A nd, as discussed below, any purchaser who received a discount in an amount greater than or equal to the improper sales tax for the purpose of offsetting the sales tax never paid the improper tax at all.
+  > 
+  > Because these purchasers did not pay the sales tax, or at least their invoices do not demonstrate that they paid the sales tax, their invoices are not sufficient evidence that any class member knew of the sales tax and then paid it. The invoices, then, are not sufficient evidence that the individualized issue of voluntary payment bars recovery on at least some claims.
+  >
+  >The fashion retailer declarations suffer from the same problem. Although the declarations generally assert that some consumers across the country were told of the sales tax issue, the declarations do not state with certainty that any member of this Alaska class was informed of the nature of the improper sales tax, was not provided a discount, and paid the sales tax nonetheless.
+  >
+  >***We do not permit a defendant to support its invocation of individualized issues with mere speculation.*** See True Health, 896 F.3d at 932. LuLaRoe's scant evidence of voluntary payment is not sufficient to defeat predominance.
+
+- [*True Health Chiropractic, Inc. v. McKesson Corp.*](https://www.westlaw.com/Document/Ia19c2f8089df11e8a5b89e7029628dd3/View/FullText.html?transitionType=Default&contextData=(sc.Default)&VR=3.0&RS=cblt1.0), 896 F.3d 923, 931–32 (9th Cir. 2018) ("Since McKesson bears the burden [on its affirmative defense of consent], we assess predominance by analyzing the consent defenses McKesson has actually advanced and for which it has presented evidence. A defendant can produce evidence of a predominance-defeating consent defense in a variety of ways. But we do not consider the consent defenses that McKesson might advance or for which it has presented no evidence.")
+'''
+
+print(dumb_to_smart_quotes_v2(test_str))
+
+# quote_str= '''
+# '' " ＂ 〃 ˮ ײ ᳓ ″ ״ ‶ ˶ ʺ “ ” ˝ ‟
+# '''
+
+# for i, quote in enumerate(quote_str.replace(' ',''), start=1):
+#     print(f"Symbol {i}: {quote}")
+#     if quote == '“':
+#         print(f"it's equal to {quote}, the {i}th one in the list")
+
+# print('“' == '“')
+# print('”' == '”')
